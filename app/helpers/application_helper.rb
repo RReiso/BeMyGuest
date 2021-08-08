b33320dcb12147851501bfe6cef10c085d144a29
Ruta R <rutareiso@protonmail.com>
2021-08-07 22:53:15 -0400

    Add helper method to create page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    main_title = "Flawless Events"
+    if page_title.empty?
+      main_title
+    else
+      page_title + " | " + main_title
+    end
+  end
+
 end
