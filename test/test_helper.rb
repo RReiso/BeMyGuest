95c4ccc6975155476b9343fe877c08374e4977fc
Ruta R <rutareiso@protonmail.com>
2021-08-19 23:32:11 -0400

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,4 +17,18 @@ class ActiveSupport::TestCase
 !session[:user_id].nil?
 end
 
+# Log in as a particular user.
+def log_in_as(user)
+session[:user_id] = user.id
+end
+
+end
+
+
+class ActionDispatch::IntegrationTest
+# Log in as a particular user.
+  def log_in_as(user, password: '1234abcd', remember_me: '1')
+    post root_path, params: { session: { email: user.email,
+    password: password, remember_me: remember_me } }
+  end
 end
