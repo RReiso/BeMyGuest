0afe8a0548548aaf044af6ab6ff48119c21fd33d
Ruta R <rutareiso@protonmail.com>
2021-09-05 06:44:42 -0400

    Add integration tests for user and event

---
+++ test/integration/events/events_create_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class EventsCreateTest < ActionDispatch::IntegrationTest
+ 
+def setup
+@user = users(:one)
+end
+
+	test 'invalid create event information' do
+		log_in_as(@user)
+get user_path(@user)
+assert_template 'users/show'
+assert_select 'a', '+ New'
+		# assert_equal before_count, after_count is same as:
+		assert_no_difference 'Event.count' do
+			post user_events_path(@user), params:
+			      { event: { 
+name: "Grandma's birthday",
+        event_date: '2021-10-09',
+        event_time: 'Not a time',
+        place: 'My home' } }
+		end
+		assert_template 'users/show'
+		assert_not flash.empty?
+    assert_select 'div.alert-danger', 'Error creating event. Please try again.'
+	end
+
+	test 'valid create event information' do
+	log_in_as(@user)
+get user_path(@user)
+assert_template 'users/show'
+assert_select 'a', '+ New'
+		# assert_equal before_count, after_count is same as:
+		assert_difference 'Event.count', 1 do
+			post user_events_path(@user), params:
+			      { event: { 
+name: "Grandma's birthday",
+        event_date: '2021-10-09',
+        event_time: '2021-10-09 12:30:00 UTC',
+        place: 'My home' } }
+		end
+		follow_redirect!
+		assert_template 'events/show'
+		assert flash.empty?
+	end
+
+
+end
--- test/integration/users/users_signup_test.rb
+++ test/integration/users/users_signup_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-	def setup
-		ActionMailer::Base.deliveries.clear
-	end
 
 	test 'invalid signup information' do
 		get signup_path
@@ -25,7 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		assert_select 'div#error_explanation'
 	end
 
-	test 'valid signup information with account activation' do
+	test 'valid signup information' do
 		get signup_path
 		assert_difference 'User.count', 1 do
 			post signup_path,
