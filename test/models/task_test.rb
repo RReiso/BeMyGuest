ec5c8e0982c104d28cadcbe45ae18ae4237b6c8f
Ruta R <rutareiso@protonmail.com>
2021-09-06 11:49:38 -0400

    Add controller and model tests for tasks

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,27 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  def setup
-    @user = users(:one)
-    @event = events(:event_one)
-    @task =
-      @event.tasks.create(
-        description: "Buy tickets"
-      )
-  end
+	def setup
+		@user = users(:one)
+		@event = events(:event_one)
+		@task = @event.tasks.create(description: 'Buy tickets')
+	end
 
-  test 'should be valid' do
-    assert @task.valid?
-  end
+	test 'should be valid' do
+		assert @task.valid?
+	end
 
-    test 'description should be present' do
-    @task.description = ' '
-    assert_not @task.valid?
-  end
+	test 'description should be present' do
+		@task.description = ' '
+		assert_not @task.valid?
+	end
 
-test 'event_id should be present' do
-    @task.event_id = ' '
-    assert_not @task.valid?
-  end
-
+	test 'event_id should be present' do
+		@task.event_id = ' '
+		assert_not @task.valid?
+	end
 end
